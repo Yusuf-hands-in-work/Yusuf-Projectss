3fb4c208c292942438cf33008016beb1d0edca10
Yusuf-hands-in-work <55299516+Yusuf-hands-in-work@users.noreply.github.com>
2019-10-03 14:09:44 -0700

    Picture Assignment

---
+++ PictureAssignment/src/Picture.java
@@ -0,0 +1,26 @@
+public class Picture
+{
+    public static void main(String[] args) {
+        System.out.println( "               _____________");
+        System.out.println( "               |           |");
+        System.out.println( "               |  *     *  |");
+        System.out.println( "               |     &     |");
+        System.out.println( "               |    ___    |");
+        System.out.println( "    -----------             -----------");
+        System.out.println( "    |         |             |         |");
+        System.out.println( "    |         |             |         |");
+        System.out.println( "    |         |             |         |");
+        System.out.println( "    |         |             |         |");
+        System.out.println( "    |         |             |         |");
+        System.out.println( "    |         |             |         |");
+        System.out.println( "    |         |             |         |");
+        System.out.println( "    ___________             ___________");
+        System.out.println( "              |      |      |          ");
+        System.out.println( "              |      |      |          ");
+        System.out.println( "              |      |      |          ");
+        System.out.println( "              |      |      |          ");
+        System.out.println( "              |      |      |          ");
+        System.out.println( "              |      |      |          ");
+        System.out.println( "              _______ _______");
+    }
+}
