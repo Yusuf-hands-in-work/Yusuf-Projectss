61f2cd5d5eced90d635a203114cb8cd9d74b22db
Yusuf-hands-in-work <55299516+Yusuf-hands-in-work@users.noreply.github.com>
2019-10-03 14:01:35 -0700

    Assignment #2

---
+++ Assignment2/src/Problem2.java
@@ -0,0 +1,15 @@
+public class Problem2
+{
+    public static void main(String[] args)
+    {
+        int room;
+        double close;
+        room = 113;
+        close = 2.71828;
+        String Subject = "Computer Science";
+        System.out.println( "This is room # " + room );
+        System.out.println( "e is close to " + close);
+        System.out.println( "I am learning a bit in " + Subject);
+    }
+
+}
---
+++ Assignment2/src/Schedule.java
@@ -0,0 +1,42 @@
+public class Schedule {
+    public static void main(String[] args) {
+
+        String c1 = "Pre Calculus 11";
+        String c2 = "Law 12";
+        String c3 = "English 11";
+        String c4 = "Automotives 12";
+        String c5 = "Active Living 11";
+        String c6 = "Chemistry 11";
+        String c7 = "Biology 11";
+        String c8 = "Programming 11";
+        String t1 = "Ms. Ungurean";
+        String t2 = "Ms. Qoun";
+        String t3 = "Mr. Trysenar";
+        String t4 = "Mr. Lai";
+        String t5 = "Mr. D";
+        String t6 = "Mr. Zitka";
+        String t7 = "Mr. Annas";
+        String t8 = "Mr. Zaremba";
+
+        System.out.println("+--------------------------------------------------+");
+        System.out.println("| 1 |          " +  c1 + "|        " + t1 + "|");
+        System.out.println("| 2 |                   " +  c2 + "|            " + t2 + "|");
+        System.out.println("| 3 |               " + c3 + "|        " + t3 + "|");
+        System.out.println("| 4 |           " + c4 + "|             " + t4 + "|");
+        System.out.println("| 5 |         " + c5 + "|               " + t5 + "|");
+        System.out.println("| 6 |             " + c6 + "|           " + t6 + "|");
+        System.out.println("| 7 |               " + c7 + "|           " + t7 + "|");
+        System.out.println("| 8 |           " + c8 + "|         " + t8 + "|");
+        System.out.println("+--------------------------------------------------+");
+
+
+
+
+
+
+
+
+
+
+    }
+}
