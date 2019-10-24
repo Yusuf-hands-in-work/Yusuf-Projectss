37b5ccdae67a9c7fef666443c02242b0b8d3e73a
Yusuf-hands-in-work <55299516+Yusuf-hands-in-work@users.noreply.github.com>
2019-10-24 14:23:33 -0700

    Encryption

---
+++ Encryption/src/Encryption.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Encryption {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System. in);
+
+        System.out.println("Please enter a word ");
+        String word = scan.next();
+        System.out.println(" ");
+        System.out.println("wwbk" + word.substring(0,3) + word + "fwjf" );
+        System.out.println(" ");
+        System.out.println(word);
+
+    }
+}
